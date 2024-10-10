b5bc8cf0503bb635ebff9afea302a2fcadc44cb8
oscarprada <oscar.prada@uao.edu.co>
2024-10-09 22:11:12 -0500

    agregar first-app ejercicio 3

---
+++ first-app/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+// Crea un archivo FirstApp.js
+import FirstApp from './FirstApp'; // Importamos el nuevo component
+
+function App() {
+  return (
+    <div className="App">
+      <FirstApp ></FirstApp>
+    </div>
+  );
+}
+
+export default App;
---
+++ first-app/src/FirstApp.js
@@ -0,0 +1,13 @@
+// src/FirstApp.js
+import React from 'react';
+
+const FirstApp = () => {
+  return (
+    <div>
+      <h1>Welcome to My First React App!</h1>
+      <span>10</span>
+    </div>
+  );
+};
+
+export default FirstApp;
