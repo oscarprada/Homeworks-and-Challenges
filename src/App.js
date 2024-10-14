8400e6efd7b01916c26358dc3d48df6404485dc9
oscarprada <oscar.prada@uao.edu.co>
2024-10-14 14:21:56 -0500

    cambio en App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from './Login';
 function App() {
   return (
     <div>
-      <h1>Bienvenido a la aplicación</h1> {/* ejercicio0 */}
+      <h1>Bienvenido a la aplicación</h1> {/* ejercicio */}
       <Login /> {/* Aquí mostramos el componente de Login */}
     </div>
   );
